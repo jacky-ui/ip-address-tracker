0d862692a0209435179e4291a88b8b318aee4fcd
jacky-ui <jacky.du.004@gmail.com>
2024-02-22 10:07:06 -0500

    create basic style and looks good so far

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.scss';
 function Header() {
     return(
         <header className='header'>
-            <h1 className='header__title'>IP ADDRESS TRACKER</h1>
+            <h1 className='header__title'>IP Address Tracker</h1>
             <SearchBar />
         </header>
     )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,12 @@
 import './SearchBar.scss';
+import submitArrow from '../../assets/images/icon-arrow.svg';
 
 function SearchBar() {
     return(
         <>
             <form className='form'>
                 <label htmlFor='ip_address'/>
-                <input name='ip_address' type='text' placeholder='Search for any IP address or domain'/>
+                <input className='form__input' name='ip_address' type='text' placeholder='Search for any IP address or domain'/>
                 <button className='form__button' type='submit' />
             </form>
         </>
