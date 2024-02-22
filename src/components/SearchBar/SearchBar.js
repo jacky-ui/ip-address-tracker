4f072a78ba0ffbf85846f73ef8976c7f22be8247
jacky-ui <jacky.du.004@gmail.com>
2024-02-22 09:51:44 -0500

    add more style to background-image on header

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,8 +3,10 @@ import './SearchBar.scss';
 function SearchBar() {
     return(
         <>
-            <form>
-                <input placeholder='Search for any IP address or domain'/>
+            <form className='form'>
+                <label htmlFor='ip_address'/>
+                <input name='ip_address' type='text' placeholder='Search for any IP address or domain'/>
+                <button className='form__button' type='submit' />
             </form>
         </>
     )
