2d5ad9a3c4b2bd6f15899896be9caece6af2080e
jacky-ui <jacky.du.004@gmail.com>
2024-02-22 22:29:39 -0500

    found the problem with the fonts. forgot to specify font-weight

--- src/components/LocationBar/LocationBar.js
+++ src/components/LocationBar/LocationBar.js
@@ -3,6 +3,24 @@ import './LocationBar.scss';
 function LocationBar() {
     return(
         <>
+            <section className='locationBar'>
+                <div>
+                    <h2 className='locationBar__title'>IP ADDRESS</h2>
+                    <p className='locationBar__text'>192.212.174.101</p>
+                </div>
+                <div>
+                    <h2 className='locationBar__title'>LOCATION</h2>
+                    <p className='locationBar__text'>Brooklyn, NY 10001</p>
+                </div>
+                <div>
+                    <h2 className='locationBar__title'>TIMEZONE</h2>
+                    <p className='locationBar__text'>UTC -05:00</p>
+                </div>
+                <div>
+                    <h2 className='locationBar__title'>ISP</h2>
+                    <p className='locationBar__text'>SPACEX Starlink</p>
+                </div>                                                
+            </section>
         </>
     )
 };
