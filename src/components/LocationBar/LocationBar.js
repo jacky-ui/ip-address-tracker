47aa41460395a0e33a7db1d09626b3e19070ffa8
jacky-ui <jacky.du.004@gmail.com>
2024-02-28 15:02:15 -0500

    looks good on desktop. only thing I don't like is the border

--- src/components/LocationBar/LocationBar.js
+++ src/components/LocationBar/LocationBar.js
@@ -4,19 +4,19 @@ function LocationBar() {
     return(
         <>
             <section className='locationBar'>
-                <div>
+                <div className='padding-border-right'>
                     <h2 className='locationBar__title'>IP ADDRESS</h2>
                     <p className='locationBar__text'>192.212.174.101</p>
                 </div>
-                <div>
+                <div className='padding-border-middle'>
                     <h2 className='locationBar__title'>LOCATION</h2>
                     <p className='locationBar__text'>Brooklyn, NY 10001</p>
                 </div>
-                <div>
+                <div className='padding-border-middle'>
                     <h2 className='locationBar__title'>TIMEZONE</h2>
                     <p className='locationBar__text'>UTC -05:00</p>
                 </div>
-                <div>
+                <div className='padding-all'>
                     <h2 className='locationBar__title'>ISP</h2>
                     <p className='locationBar__text'>SPACEX Starlink</p>
                 </div>                                                
