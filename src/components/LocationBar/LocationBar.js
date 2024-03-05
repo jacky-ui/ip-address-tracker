bcb52f825cb36d957948f3c1f78e6ac1951fd927
jacky-ui <jacky.du.004@gmail.com>
2024-03-04 19:40:14 -0500

    works now to map address titles. else if got applied to last else due to syntax error. forgot to apply comparison for each condition

--- src/components/LocationBar/LocationBar.js
+++ src/components/LocationBar/LocationBar.js
@@ -1,25 +1,53 @@
+import { useState, useEffect } from 'react';
+import LocationInfo from '../LocationInfo/LocationInfo';
 import './LocationBar.scss';
 
 function LocationBar() {
+    const [ipAddress, setipAddress] = useState('192.212.174.101');
+    const [location, setlocation] = useState('Brooklyn, NY 10001');
+    const [timezone, settimezone] = useState('UTC -05:00');
+    const [isp, setisp] = useState('SPACEX Starlink')    
+
+    let locationDefault = [
+        {
+            title: 'IP ADDRESS',
+            locationInfo: ipAddress
+        },
+        {
+            title: 'LOCATION',
+            locationInfo: location
+        },
+        {
+            title: 'TIMEZONE',
+            locationInfo: timezone
+        },
+        {
+            title: 'ISP',
+            locationInfo: isp
+        },
+    ]
+
+    let checkForClass = (propInfo) => {
+        let paddingClass = ''
+        console.log(propInfo === 'ISP')
+        if(propInfo === 'IP ADDRESS') {
+            return paddingClass = 'padding-border-right';
+        } else if(propInfo === 'TIMEZONE' || propInfo === 'LOCATION') {
+            return paddingClass = 'padding-border-middle';
+        } else return paddingClass = 'padding-all';
+    } ;
+
     return(
         <>
             <section className='locationBar'>
-                <div className='padding-border-right'>
-                    <h2 className='locationBar__title'>IP ADDRESS</h2>
-                    <p className='locationBar__text'>192.212.174.101</p>
-                </div>
-                <div className='padding-border-middle'>
-                    <h2 className='locationBar__title'>LOCATION</h2>
-                    <p className='locationBar__text'>Brooklyn, NY 10001</p>
-                </div>
-                <div className='padding-border-middle'>
-                    <h2 className='locationBar__title'>TIMEZONE</h2>
-                    <p className='locationBar__text'>UTC -05:00</p>
-                </div>
-                <div className='padding-all'>
-                    <h2 className='locationBar__title'>ISP</h2>
-                    <p className='locationBar__text'>SPACEX Starlink</p>
-                </div>                                                
+                {locationDefault.map((location) => (
+                    <LocationInfo
+                        classFunction={checkForClass} 
+                        title={location.title}
+                        locationInfo={location.locationInfo}
+                        key={location.title}
+                    />
+                ))}                                            
             </section>
         </>
     )
--- src/components/LocationInfo/LocationInfo.js
+++ src/components/LocationInfo/LocationInfo.js
@@ -1,8 +1,12 @@
 import './LocationInfo.scss';
 
-function LocationInfo() {
+function LocationInfo({ title, locationInfo, classFunction }) {
     return(
         <>
+            <div className={classFunction(title)}>
+                <h2 className='locationBar__title'>{title}</h2>
+                <p className='locationBar__text'>{locationInfo}</p>
+            </div>
         </>
     )
 };
