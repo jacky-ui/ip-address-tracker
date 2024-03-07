64f8fe338a73b7715a313df0525ae5b5f4864652
jacky-ui <jacky.du.004@gmail.com>
2024-03-07 14:49:08 -0500

    add api key and also moved state to appjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/Header/Header';
 import LocationBar from './components/LocationBar/LocationBar';
 import Map from './components/Map/Map';
 import './styles/App.css';
 
 function App() {
+  const [ipAddress, setipAddress] = useState('192.212.174.101');
+  const [location, setlocation] = useState('Brooklyn, NY 10001');
+  const [timezone, settimezone] = useState('UTC -05:00');
+  const [isp, setisp] = useState('SPACEX Starlink');   
+  
+  let locationDefault = [
+    {
+        title: 'IP ADDRESS',
+        locationInfo: ipAddress
+    },
+    {
+        title: 'LOCATION',
+        locationInfo: location
+    },
+    {
+        title: 'TIMEZONE',
+        locationInfo: timezone
+    },
+    {
+        title: 'ISP',
+        locationInfo: isp
+    },
+]
+
   return (
     <>
       <Header />
-      <LocationBar />
+      <LocationBar 
+        locationDefault={locationDefault}
+      />
       <Map />
     </>
   );
--- src/components/LocationBar/LocationBar.js
+++ src/components/LocationBar/LocationBar.js
@@ -1,32 +1,8 @@
-import { useState, useEffect } from 'react';
 import LocationInfo from '../LocationInfo/LocationInfo';
 import './LocationBar.scss';
 
-function LocationBar() {
-    const [ipAddress, setipAddress] = useState('192.212.174.101');
-    const [location, setlocation] = useState('Brooklyn, NY 10001');
-    const [timezone, settimezone] = useState('UTC -05:00');
-    const [isp, setisp] = useState('SPACEX Starlink')    
-
-    let locationDefault = [
-        {
-            title: 'IP ADDRESS',
-            locationInfo: ipAddress
-        },
-        {
-            title: 'LOCATION',
-            locationInfo: location
-        },
-        {
-            title: 'TIMEZONE',
-            locationInfo: timezone
-        },
-        {
-            title: 'ISP',
-            locationInfo: isp
-        },
-    ]
-
+function LocationBar({ locationDefault }) {
+    console.log(locationDefault)
     let checkForClass = (propInfo) => {
         let paddingClass = ''
         if(propInfo === 'IP ADDRESS') {
