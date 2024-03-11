044f683a69278e38b4068473791fe84d74e6ffa9
jacky-ui <jacky.du.004@gmail.com>
2024-03-11 11:29:57 -0400

    remove unused if else function

--- src/App.js
+++ src/App.js
@@ -6,29 +6,34 @@ import Map from './components/Map/Map';
 import './styles/App.css';
 
 function App() {
-  const [addressData, setAddressData] = useState(null) 
+  const [addressData, setAddressData] = useState(false) 
 
   const API_WEBSITE = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`;
 
   useEffect(() => {
     axios
-      .get(`${API_WEBSITE}&ipAddress=${ipAddress}`)
+      .get(`${API_WEBSITE}&ipAddress=192.212.174.101`)
       .then((response) => {
         setAddressData(response.data);
-        console.log(addressData.location)
       })
   }, []);
 
   return (
     <>
       <Header />
-      <LocationBar 
-        addressInfo={addressData}
-      />
-      <Map 
-        lat={addressData.location.lat}
-        long={addressData.location.lng}
-      />
+      {
+        addressData ? <div>Loading info....</div> :
+        <LocationBar 
+          addressInfo={addressData}
+        />
+      }
+      {
+        addressData ? <div>Map is loading</div> : 
+        <Map 
+          lat={addressData.location.lat}
+          long={addressData.location.lng}
+        />
+      }
     </>
   );
 }
--- src/components/LocationBar/LocationBar.js
+++ src/components/LocationBar/LocationBar.js
@@ -2,15 +2,6 @@ import LocationInfo from '../LocationInfo/LocationInfo';
 import './LocationBar.scss';
 
 function LocationBar({ addressInfo }) {
-    // let checkForClass = (propInfo) => {
-    //     let paddingClass = ''
-    //     if(propInfo === 'IP ADDRESS') {
-    //         return paddingClass = 'padding-border-right';
-    //     } else if(propInfo === 'TIMEZONE' || propInfo === 'LOCATION') {
-    //         return paddingClass = 'padding-border-middle';
-    //     } else return paddingClass = 'padding-all';
-    // } ;
-
     return(
         <>
             <section className='locationBar'>
